9c2003578712c86b70fb72022957e8cdebc78b18
sdesu24 <desu.sindhu24@gmail.com>
2019-11-28 18:25:47 -0600

    created HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=0,y=0,z=0;
+		System.out.println("The input is :x= "+x+" y= "+y+" z= "+z);
+		x++;
+		y=y+2;
+		z=x+y;
+		System.out.println("The output is :"+z);
+	}
+
+}
